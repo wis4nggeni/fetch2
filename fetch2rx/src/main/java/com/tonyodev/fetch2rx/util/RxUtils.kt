41a5f51662117a55eeb9d1c74593f1b56d4ee944
wis4nggeni <61108897+wis4nggeni@users.noreply.github.com>
2022-06-28 07:58:10 +0700

    Delete fetch2rx directory

--- fetch2rx/src/main/java/com/tonyodev/fetch2rx/util/RxUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.tonyodev.fetch2rx.util
-
-import com.tonyodev.fetch2rx.Convertible
-import io.reactivex.Flowable
-
-fun <T> Flowable<T>.toConvertible(): Convertible<T> {
-    return Convertible(this)
-}
